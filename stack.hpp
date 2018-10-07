207417ba0feb69a318bc500cb5f4a4fe27c9b0c6
rusdevops <rusdevops@gmail.com>
2018-10-07 14:14:43 +0300

    Create stack.hpp

---
+++ stack.hpp
@@ -0,0 +1,13 @@
+#include <stack>
+
+template <typename T>
+class stack
+{
+  public:
+    void push(T&& value) { stack.push(value); }
+    void push(T value) { stack.push(value); }
+    void pop() { stack.pop(); }
+    const T& head() const { return stack.top(); }
+  private:
+    std::stack<T> stack;
+};
