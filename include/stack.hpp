ebec5210eda45643d24894ba444acd8c4c8b7837
rusdevops <rusdevops@gmail.com>
2018-10-08 00:11:47 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,20 +1,6 @@
-// Copyright 2018 Vyacheslav Vershinin
+// Copyright 2018 Your Name <your_email>
 
 #ifndef INCLUDE_STACK_HPP_
 #define INCLUDE_STACK_HPP_
-
-#include <stack>
-
-template <typename T>
-class stack
-{
-public:
-  void push(T&& value) { stack.push(value); }
-  void push(T value) { stack.push(value); }
-  void pop() { stack.pop(); }
-  const T& head() const { return stack.top(); }
-private:
-  std::stack<T> stack;
-};
 
 #endif // INCLUDE_STACK_HPP_
